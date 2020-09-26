f3c6eb7046fa3673e9421dc0f34a29f9a3ed4761
o-atsu <atsugundam@gmail.com>
2020-09-26 18:49:16 +0900

    target_cam
    object3Dに追従するカメラ

--- atsu_samples/index.js
+++ atsu_samples/index.js
@@ -1,4 +1,7 @@
 import * as camSwitcher from "./camera/camSwitcher.js";
+import * as camUtils from "./camera/camUtils.js";
+import {FBXLoader} from "./three.js-master/examples/jsm/loaders/FBXLoader.js";
+import {GLTFLoader} from "./three.js-master/examples/jsm/loaders/GLTFLoader.js";
 window.addEventListener('load', init);
 
 
@@ -28,12 +31,63 @@ function init(){
 	//////// scene
 	const testscene = MakeTestScene();
 
+	//////// FBXLoad
+	const steel_frame = "./models/steel_frame.fbx";
+	initFBX(steel_frame, new THREE.Vector3(0.02,0,0), new THREE.Vector3(0,0,0), new THREE.Vector3(0.06, 0.06, 0.06));
+
+	function initFBX(File, pos, rot, sca){
+		const loader = new FBXLoader();
+		loader.load(File, (fbx) => {
+			fbx.traverse((child) => {
+				if(child.isMesh){
+					child.castShadow = true;
+					child.recieveShadow = true;
+				}
+			});
+
+			fbx.position.set(pos.x, pos.y, pos.z);
+			console.log(fbx.position);
+			fbx.rotation.set(rot.x, rot.y, rot.z);
+			fbx.scale.set(sca.x, sca.y, sca.z);
+
+			testscene.add(fbx);
+
+			camera = new camUtils.target_cam(new THREE.Vector3(40, 50, 30), fbx, new THREE.Vector3(0, 30, 0));
+
+			let t = 0.0;
+			Update();
+			function Update(){
+				requestAnimationFrame(Update);
+				fbx.position.add(new THREE.Vector3(0.0, Math.sin(t), 0.0));
+
+				t += 0.5;
+			}
+
+		});
+	}
+	function initGLTF(File, pos, rot, sca){
+		const loader = new GLTFLoader();
+		loader.load(File, (gltf) => {
+			let model = gltf.scene;
+
+			model.position.set(pos.x, pos.y, pos.z);
+			model.rotation.set(rot.x, rot.y, rot.z);
+			model.scale.set(sca.x, sca.y, sca.z);
+
+			testscene.add(model);
+		});
+	}
+
+
+
+	//////// renderer
+	//const ef =  new THREE.EffectComposer(renderer);
 	renderer.render(testscene, camera);
 	Update();
 
 	//////// Update every frame
 	function Update(){
-		camera = swt.now_cam();
+//		camera = swt.now_cam();
 		renderer.render(testscene, camera);
 
 		requestAnimationFrame(Update);
@@ -61,19 +115,19 @@ function MakeTestScene(){
 	const sphere = new THREE.SphereGeometry(5,32,32);
 
 	// spheres
-	const c = new THREE.Mesh(sphere, white);
-	const x = new THREE.Mesh(sphere, red);
-	const y = new THREE.Mesh(sphere, green);
-	const z = new THREE.Mesh(sphere, blue);
-	c.position.set(0,0,0);
-	x.position.set(20,0,0);
-	y.position.set(0,20,0);
-	z.position.set(0,0,20);
+//	const c = new THREE.Mesh(sphere, white);
+//	const x = new THREE.Mesh(sphere, red);
+//	const y = new THREE.Mesh(sphere, green);
+//	const z = new THREE.Mesh(sphere, blue);
+//	c.position.set(0,0,0);
+//	x.position.set(20,0,0);
+//	y.position.set(0,20,0);
+//	z.position.set(0,0,20);
 
 	// lights
 	const light = new THREE.DirectionalLight(0xffffff);
 
-	light.position.set(1,1,1);
+	light.position.set(1,10,1);
 
 	// helpers
 	const gridHelper = new THREE.GridHelper(200,50);
@@ -82,10 +136,10 @@ function MakeTestScene(){
 
 	// scene
 	const scene = new THREE.Scene();
-	scene.add(c);
-	scene.add(x);
-	scene.add(y);
-	scene.add(z);
+//	scene.add(c);
+//	scene.add(x);
+//	scene.add(y);
+//	scene.add(z);
 	scene.add(light);
 	scene.add(gridHelper);
 	scene.add(axisHelper);
